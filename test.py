46a476ba5cf58c602f4e1b776de326bab740c936
JDilger <32108051+jdilger@users.noreply.github.com>
2021-10-26 22:23:05 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # test.py
 
 from contributions.render_html import create_graph
-test = create_graph([r"test_repo_count.txt"])
+test = create_graph([r"repo_count.txt"])
 print(type(test))
 
 with open('test.html','w') as f:
